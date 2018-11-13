7eb68e0eafa0eb4d7d412a12b112eb043629d550
Isaak <imoore1@mail.pima.edu>
2018-11-12 23:05:32 -0700

    Changed project layout and added canvas

--- Assets/Scripts/DestroyRB.cs
+++ Assets/Scripts/DestroyRB.cs
@@ -1,14 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DestroyRB : MonoBehaviour {
+
+    public Text countText;
+    public Text winText;
+    private int count;
+
+    private void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pickup"))
         {
             Destroy(other.gameObject);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
