bb5cb0790559fd8121e25b1dffedede91b7e15ca
Isaak <imoore1@mail.pima.edu>
2018-11-12 22:51:24 -0700

    Made Followers into Prefab and made separate script
    Fixed problem of Followers being unable to be collected, by converting them into a prefab, creating a separate collecting script that destroys the object instead of turning off the rigid body, turned Is Trigger on sphere collider, then removed the rigid body because not only was it no longer needed, but it would actually make the Followers appear invisible when Is Trigger was enabled.

---
+++ Assets/Scripts/DestroyRB.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRB : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
